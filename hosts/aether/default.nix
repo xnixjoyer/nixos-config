76e18ed18b5672deeb7c7da7c1540b5f8be8d261
nixjoyer <Error5634@proton.me>
2026-07-17 18:50:54 +0200

    Add udev rules for NVMe scheduler
    Add udev rules for NVMe block devices to use mq-deadline scheduler.

--- hosts/aether/default.nix
+++ hosts/aether/default.nix
@@ -11,6 +11,17 @@
   boot.kernelPackages =
     pkgs.cachyosKernels.linuxPackages-cachyos-bore-x86_64-v3;
 
+services.udev.extraRules = ''
+  ACTION=="add|change", SUBSYSTEM=="block", KERNEL=="nvme[0-9]n[0-9]", ATTR{queue/scheduler}="mq-deadline"
+'';
+
+
+
+#boot.initrd.luks.devices."luks-28ccacbc-d315-4bc1-9b89-48d99076132e" = {
+#  bypassWorkqueues = true;
+#  allowDiscards = true;
+#};
+
   hardware.cpu.intel.updateMicrocode = true;
   services.thermald.enable = true;
 }
